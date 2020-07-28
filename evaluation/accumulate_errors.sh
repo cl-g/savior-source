8af72ce11aa528515c9f32d8f32888262afbefb2
Leon Bettscheider <bettscheider@google.com>
2020-07-28 13:03:17 +0000

    Eval scripts: (1) Improved accumulate_errors.py (2) Add regex-runtime-ubsan.py, which lists bugs from UBSAN logs (not available for SAVIOR ubsan)

--- evaluation/accumulate_errors.sh
+++ evaluation/accumulate_errors.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ROOT_DIR=/root/git/savior-source/tests/libxml2-target-libFuzzer/libxml2-2.9.7/target-obj-savior
+ROOT_DIR=/root/git/savior-source/tests/sqlite3-savior-libFuzzer/sqlite3-2/build/obj-savior-bigger
 CORPUS_DIR=$ROOT_DIR/corpus
-SAVIOR_BINARY=$ROOT_DIR/savior-target
+#this is actually the libfuzzer binary
+SAVIOR_BINARY=$ROOT_DIR/savior-sqlite3
 STDERR_LOG_DIR=$ROOT_DIR/evaluation_dir/stderr_logs
 
 rm -rf $STDERR_LOG_DIR
@@ -13,11 +14,20 @@ for FULL_PATH in $CORPUS_DIR/*; do
 	[ -e "$FULL_PATH" ] || continue
 	FILE_NAME="${FULL_PATH##*/}"
      	echo "F: $FILE_NAME"
-	$SAVIOR_BINARY < "$FULL_PATH" 2>&1 >/dev/null | grep "error" > "$STDERR_LOG_DIR/stderr-$FILE_NAME"
+     	echo "P: $FULL_PATH"
+	if [ -f "${FULL_PATH}" ] ; then #must be file, not dir
+		# Uncomment savior binary/fuzzer, depending on if we want to reproduce the violations triggered in the fuzzer or savior build.
+		# libFuzzer's UBSAN is newer, and by default fsanitize=integer covers more cases (e.g. implicit casts).
+		#savior binary:
+		$SAVIOR_BINARY < $FULL_PATH 2>&1 >/dev/null | grep "error" > "$STDERR_LOG_DIR/stderr-$FILE_NAME"
+		#fuzzer:
+		#/root/git/savior-source/tests/libpcap_paper_libFuzzer/build/fuzz_pcap $FULL_PATH 2>&1 >/dev/null | grep "error" > "$STDERR_LOG_DIR/stderr-$FILE_NAME"
+	fi
 done
 
 # Deduplicate the bugs
-cat $STDERR_LOG_DIR/* > $STDERR_LOG_DIR/combined
+#cat $STDERR_LOG_DIR/* > $STDERR_LOG_DIR/combined
+find $STDERR_LOG_DIR -type f -name "*" -exec cat '{}' ';' > $STDERR_LOG_DIR/combined
 python2 deduplicate_regex.py "$STDERR_LOG_DIR/combined" > $STDERR_LOG_DIR/unique_bugs
 
 echo "All bugs found:"
--- evaluation/regex-runtime-ubsan.py
+++ evaluation/regex-runtime-ubsan.py
@@ -6,19 +6,20 @@ import re
 # jdarith.c:308:53: runtime error: left shift of negative value -7
 # Same bug, but uniq cannot deduplicate this.
 # We take the first word after "runtime error: " to differentiate between different bug categories on the same line.
-
 def main():
 	assert len(sys.argv) == 2, "argc"
 	f = open(sys.argv[1], 'r')
 	lines = f.readlines() 
 	s = set()
-	pattern = re.compile("(.+):(\d+):(\d+): runtime error: (\w+).*")
+	pattern = re.compile("^(.*\.c):(\d+):(\d+):\sruntime error:\s(\w+).*$")
 	for l in lines:
 		m = pattern.search(l)
 		if m:
 			s.add((m.group(1), m.group(2), m.group(3), m.group(4)))
 
-        print("Set: " + "\n".join([str(m) for m in s]))
+	sorted_by_line = sorted(s, key=lambda tup: tup[1])
+        print("Set: " + "\n".join([str(m) for m in sorted_by_line]))
+        #print("Set: " + "\n".join([str(m) for m in s]))
 	print("Size of set: {}".format(len(s)))
 			
 
